111cd007ce18396f82b6cc5ff8c5856caf766eb7
geek_731 <kami0731mae@gmail.com>
2021-01-19 15:50:46 +0900

    update

--- README.md
+++ README.md
@@ -70,10 +70,10 @@ PC: Program counter
 ## Asm
 An assembler for this RISC16 arch written in Rust. This assmbler generates binary in ascii text.
 
-`asm <source file>` or `asm <source file> -o <output file>`
+`asm-risc16 <source file>` or `asm <source file> -o <output file>`
 
 If you don't specify the output file, this program will generate sim_risc16.mem.
 
-The Makefile in risc16 directory is only support sim_risc16.mem for simulation.
+The Makefile in risc16 directory support sim_risc16.mem for simulation.
 
 `make sim` is the command to execute the simulation.
---
+++ asm-risc16/target/debug/deps/lazy_static-31f591a6bfa1bad3.d
@@ -0,0 +1,6 @@
+/Users/geek_731/Documents/Univ/Exp4/RISC16/asm-risc16/target/debug/deps/lazy_static-31f591a6bfa1bad3.rmeta: /Users/geek_731/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs /Users/geek_731/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs
+
+/Users/geek_731/Documents/Univ/Exp4/RISC16/asm-risc16/target/debug/deps/lazy_static-31f591a6bfa1bad3.d: /Users/geek_731/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs /Users/geek_731/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs
+
+/Users/geek_731/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs:
+/Users/geek_731/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs:
